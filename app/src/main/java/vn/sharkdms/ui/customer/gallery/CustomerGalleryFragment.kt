239cbe919183d8bfe4f02f96de052bd68f00bf7d
Noah <doquockhanhan@gmail.com>
2021-10-12 23:58:22 +0700

    create Gallery function for Customer

---
+++ app/src/main/java/vn/sharkdms/ui/customer/gallery/CustomerGalleryFragment.kt
@@ -0,0 +1,7 @@
+package vn.sharkdms.ui.customer.gallery
+
+import androidx.fragment.app.Fragment
+import vn.sharkdms.R
+
+class CustomerGalleryFragment : Fragment(R.layout.fragment_customer_gallery) {
+}
