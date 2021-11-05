558a6aa6fbd6dfa18569f2177b03e04249b02baf
Noah <doquockhanhan@gmail.com>
2021-11-05 17:02:27 +0700

    remove log code

--- app/src/main/java/vn/sharkdms/ui/customer/info/CustomerInfoFragment.kt
+++ app/src/main/java/vn/sharkdms/ui/customer/info/CustomerInfoFragment.kt
@@ -122,7 +122,6 @@ class CustomerInfoFragment : Fragment(R.layout.fragment_customer_info) {
     }
 
     private fun bind(binding: FragmentCustomerInfoBinding) {
-        Toast.makeText(requireContext(), args.toString(), Toast.LENGTH_LONG).show()
         binding.apply {
             if (args.customer.customerAvatar.isNotEmpty()) {
                 Glide.with(this@CustomerInfoFragment).load(args.customer.customerAvatar).circleCrop()
